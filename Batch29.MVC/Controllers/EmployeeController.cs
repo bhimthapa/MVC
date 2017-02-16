f455c876d5d3d10f2cd2f8c85c4f790db3749e93
Raghunath <Raghunath@RGSS-PC>
2017-02-15 19:34:30 -0500

    Added update and delete options

--- Batch29.MVC/Controllers/EmployeeController.cs
+++ Batch29.MVC/Controllers/EmployeeController.cs
@@ -1,6 +1,7 @@
 ﻿using Batch29.MVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -31,42 +32,64 @@ namespace Batch29.MVC.Controllers
         }
         public ActionResult AuthenticateUserST(LoginData data, ActionType myActionType)
         {
-            if(ActionType.LoginST==myActionType)
+            var db = new Users();
+            if (ActionType.LoginST==myActionType)
             {
+                bool isAuthorize = db.UserLogins
+                .Where(x => x.UserName == data.UserId && x.Password == data.Password)
+                .Any();
 
+                if (isAuthorize)
+                {
+                    return View("Home");
+                }
+                else
+                {
+                    ViewBag.errorMsg = "Invalid User Name or Password";
+                    return View("LoginST");
+                }
             }
             else if(ActionType.Add == myActionType)
             {
-
+                User user = new MVC.User();
+                user.UserName = data.UserId;
+                user.Password = data.Password;
+                user.UserProfileId = 101;
+                db.UserLogins.Add(user);
+                db.SaveChanges();
             }
             else if (ActionType.Update == myActionType)
             {
+                
+                var result = db.UserLogins.SingleOrDefault(b => b.UserName == data.UserId);
+                if (result != null)
+                {
+                    
+                    result.UserName = data.UserId;
+                    result.Password = data.Password;
+                    db.Entry(result).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+               // db.SaveChanges();
 
             }
             else if (ActionType.Delete == myActionType)
             {
-
+                var result = db.UserLogins.SingleOrDefault(b => b.UserName == data.UserId);
+                if (result != null)
+                {
+                    db.Entry(result).State = EntityState.Deleted;
+                    db.SaveChanges();
+                }
             }
             if (!ModelState.IsValid)
             {
                 return View("LoginST",data);
             }
 
-            var db = new Users();
-            bool isAuthorize = db.UserLogins
-                .Where(x => x.UserName == data.UserId && x.Password == data.Password)
-                .Any();
 
-            if(isAuthorize)
-            {
-                return View("Home"); 
-            }
-            else
-            {
-                ViewBag.errorMsg = "Invalid User Name or Password";
-                return View("LoginST");
-            }
-            
+
+            return View();
         }
 
         public ActionResult Home()
